5ae36f36d9429fb1d239b766be565a96ff960389
abhishek thakur <abhishek4@gmail.com>
2022-12-31 20:59:08 +0000

    fix

--- setup.py
+++ setup.py
@@ -52,7 +52,10 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     keywords="diffuzers diffusers",
+    include_package_data=True,
 )
